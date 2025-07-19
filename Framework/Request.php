342663f6beb21c152311eb1c3c8fa4fc6de2569d
BdsOliveira <bds.commus@gmail.com>
2025-07-19 10:24:31 -0300

    add request handler

---
+++ Framework/Request.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Framework;
+
+class Request
+{
+    public static function method(): string
+    {
+        return $_SERVER['REQUEST_METHOD'] ?? 'GET';
+    }
+
+    public static function path(): string
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/';
+    }
+
+    public static function query(): string
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY) ?? '';
+    }
+}
