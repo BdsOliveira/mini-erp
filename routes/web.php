b46fb882d3d81bb985342efd803a29a08722cdc1
BdsOliveira <bds.commus@gmail.com>
2025-07-19 10:23:15 -0300

    add basic routes

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\CuponsController;
+use App\Http\Controllers\EstoqueController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PedidosController;
+use App\Http\Controllers\ProdutosController;
+
+return [
+    "GET" => [
+        "/" => [HomeController::class, "index"],
+
+        "/pedidos" => [PedidosController::class, "index"],
+        "/pedidos/criar" => [PedidosController::class, "create"],
+
+        "/produtos" => [ProdutosController::class, "index"],
+        "/produtos/criar" => [ProdutosController::class, "create"],
+
+        "/cupons" => [CuponsController::class, "index"],
+        "/cupons/criar" => [CuponsController::class, "create"],
+
+        "/estoque" => [EstoqueController::class, "index"],
+        "/estoque/criar" => [EstoqueController::class, "create"],
+    ],
+    "POST" => [
+        "/pedidos" => [PedidosController::class, "store"],
+        
+        "/produtos" => [ProdutosController::class, "store"],
+
+        "/cupons" => [CuponsController::class, "store"],
+
+        "/estoque" => [EstoqueController::class, "store"],
+    ],
+];
