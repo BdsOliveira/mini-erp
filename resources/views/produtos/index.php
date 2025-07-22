d833b97b9d1604e281093bc5f024bb8cb70574d6
BdsOliveira <bds.commus@gmail.com>
2025-07-22 00:28:10 -0300

    add listing products

--- resources/views/produtos/index.php
+++ resources/views/produtos/index.php
@@ -3,5 +3,28 @@
 {% block title %}Produtos{% endblock %}
 
 {% block content %}
-<h1>Produtos</h1>
-{% endblock %}
+<div class="d-flex">
+    <h1>Produtos</h1>
+    <div class="" style="margin-left: auto">
+        <a href="/produtos/criar" class="btn btn-primary">Criar produto</a>
+    </div>
+</div>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Preço</th>
+        </tr>
+    </thead>
+    <tbody>
+        {% for product in products %}
+        <tr>
+            <td>{{ product.id }}</td>
+            <td>{{ product.nome }}</td>
+            <td>{{ product.preco }}</td>
+        </tr>
+        {% endfor %}
+    </tbody>
+    {% endblock %}
