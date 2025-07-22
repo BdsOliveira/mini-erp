699872d3fadfb997e0ab982d65f02b00ea4ae377
BdsOliveira <bds.commus@gmail.com>
2025-07-22 20:12:57 -0300

    handle slashes at path route end

--- framework/Http/Request.php
+++ framework/Http/Request.php
@@ -13,7 +13,7 @@ class Request
 
     public static function path(): string
     {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/';
+        return parse_url(rtrim($_SERVER['REQUEST_URI'], '/'), PHP_URL_PATH) ?? '/';
     }
 
     public static function query(): string
