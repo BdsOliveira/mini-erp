4c574a938ed2204547baec5d5eff292d6fad599b
BdsOliveira <bds.commus@gmail.com>
2025-07-22 00:23:02 -0300

    add functions on Request to get one, all and images attributes

--- framework/Http/Request.php
+++ framework/Http/Request.php
@@ -20,4 +20,31 @@ class Request
     {
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY) ?? '';
     }
+
+    public static function get(string $key): string
+    {
+        return htmlspecialchars($_REQUEST[$key] ?? '', ENT_QUOTES, 'UTF-8');
+    }
+
+    public static function all(): array
+    {
+        $data = [];
+        foreach ($_REQUEST as $key => $value) {
+            $data[$key] = self::get(key: $key);
+        }
+        return $data;
+    }
+
+    public static function image(string $key): string
+    {
+        if (!isset($_FILES[$key])) {
+            return '';
+        }
+
+        if ($_FILES[$key]['error'] !== UPLOAD_ERR_OK) {
+            return '';
+        }
+
+        return "data:{$_FILES[$key]['type']};base64,{base64_encode(file_get_contents($_FILES[$key]['tmp_name']))}";
+    }
 }
